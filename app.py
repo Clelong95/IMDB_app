353b2f1c70ffde3844585995cd0a9aa8f5f023e6
Clelong95 <lelongclem@gmail.com>
2020-12-15 13:02:00 +0100

    Added new plots

--- app.py
+++ app.py
@@ -24,31 +24,45 @@ server = app.server
 
 #---------------- Simple plots (no callbacks) ------------------------------------
 year_hist = px.histogram(df,x="Year",nbins=100, title="Years histogram")
+
 note_pie = px.pie(df, names='User_note',title='Note repartition')
+note_pie.update_traces(textposition='inside', textinfo='percent+label')
+
+type_pie = px.pie(df, names='Type',title='Type repatition')
+type_pie.update_traces(textposition='inside', textinfo='percent+label')
+
+L_genre_temp = [i.split() for i in df['Genre'].values]
+L_genre = [x for j in L_genre_temp for x in j]
+
+df_genre = pd.DataFrame(L_genre).rename({0:'Genre'},axis=1)
+genre_pie = px.pie(df_genre, names = "Genre", title="Genre repartition")
+genre_pie.update_traces(textposition='inside', textinfo='percent+label', showlegend= False)
+
 #---------------- App Layout ------------------------------------
 
-app.layout = html.Div(
+app.layout = html.Div(children=
     [
-        dbc.Row(dbc.Col(dcc.Graph(id='year_hist',figure=year_hist))),
+    	dbc.Row(dbc.Col(html.H1("IMDd Dashboard", style={"textAlign":"center"}))),
         dbc.Row(
             [
-                dbc.Col(dcc.Slider(id='director_slider', min = 1, max = min(25,df['Director'].nunique()), marks={str(i): str(i) for i in range(min(25,df['Director'].nunique()))}, value= 5, vertical=True), width = 1),
                 dbc.Col(dcc.Graph(id='director_1')),
+                dbc.Col(dcc.Slider(id='director_slider', min = 1, max = min(25,df['Director'].nunique()), marks={str(i): str(i) for i in range(min(25,df['Director'].nunique()))}, value= 5, vertical=True), width = 1),
                 dbc.Col(dcc.Graph(id='director_2')),
             ]
         ),
         dbc.Row(
             [
-                dbc.Col(dcc.Slider(id='actor_slider', min = 1, max = min(25,df_2['Actor'].nunique()), marks={str(i): str(i) for i in range(min(25,df_2['Actor'].nunique()))}, value= 5, vertical=True), width = 1),
                 dbc.Col(dcc.Graph(id='actor_1')),
+                dbc.Col(dcc.Slider(id='actor_slider', min = 1, max = min(25,df_2['Actor'].nunique()), marks={str(i): str(i) for i in range(min(25,df_2['Actor'].nunique()))}, value= 5, vertical=True), width = 1),
                 dbc.Col(dcc.Graph(id='actor_2')),
             ]
         ),
+        dbc.Row(dbc.Col(dcc.Graph(id='year_hist',figure=year_hist))),
         dbc.Row(
             [
                 dbc.Col(dcc.Graph(id='note_pie', figure=note_pie)),
-                dbc.Col(dcc.Graph()),
-                dbc.Col(dcc.Graph()),
+                dbc.Col(dcc.Graph(id='type_pie', figure=type_pie)),
+                dbc.Col(dcc.Graph(id='genre_pie', figure=genre_pie)),
             ]
         )
 
@@ -70,7 +84,8 @@ def update_director(input_value):
     df_directors_note = pd.DataFrame(df[df['Director'].isin(df_directors.index)].groupby('Director').mean()['User_note']).sort_values('User_note',ascending=False)
     fig_2 = go.Figure(data=[go.Table(
     header=dict(values=['Director', 'Average Note'],
-                fill_color='paleturquoise',
+                fill_color='#1f77b4',
+                font=dict(color='white'),
                 align='left'),
     cells=dict(values=[df_directors_note.index, round(df_directors_note.User_note,2)],
                fill_color='lavender',
@@ -95,7 +110,8 @@ def update_actor(input_value):
 
     fig_2 = go.Figure(data=[go.Table(
     header=dict(values=['Actor', 'Average Note'],
-                fill_color='paleturquoise',
+                fill_color='#1f77b4',
+                font=dict(color='white'),
                 align='left'),
     cells=dict(values=[df_actors_note.index, round(df_actors_note.User_note,2)],
                fill_color='lavender',
@@ -104,6 +120,13 @@ def update_actor(input_value):
     fig_2.update_layout(title = "Top rated Actors" , transition_duration=500)
 
     return fig_1,fig_2
+
+#@app.callback(
+#	Output('year_hist','figure'),
+#	Output('note_pie','figure'),
+#	Output('type_pie','figure'),
+#	Output('genre_pie','figure'),
+#	Input())
 
 
 if __name__ == '__main__':
