2bdc02ec15fffb7c8d920d3b6f95fa14f5b05f7c
Clelong95 <lelongclem@gmail.com>
2021-01-14 13:05:43 +0100

    Change to multi page organisation

---
+++ apps/explorer.py
@@ -0,0 +1,67 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+from app import app
+
+img_style = {"width":"8%","margin":"1%"}
+
+layout = html.Div(children=
+	[
+		dbc.Row(
+			[ 
+				dbc.Col(html.H2("Movies explorer")),
+			]
+		),
+		dbc.Row(
+			[
+				dbc.Col(dcc.Checklist(id="type_checklist",
+									  options=[{'label': 'Movie', 'value': 'Movie'},
+            									{'label': 'Serie', 'value': 'Serie'}],
+            						  value=['Movie','Serie'])),
+				dbc.Col(dcc.Dropdown(id="director_dropdown",value=[],multi=True)),
+				dbc.Col(dcc.Dropdown(id="actor_dropdown", value=[], multi= True))
+			]
+		),
+		dbc.Row(id="poster_row"),
+	]
+)
+
+@app.callback(
+	Output('director_dropdown','options'),
+	Output('actor_dropdown','options'),
+	Input('intermediate-value','children'))
+def update_dropdowns(df_json):
+	df = pd.read_json(df_json)
+	director_options =  [{"label":i,"value":i} for i in df.Director.unique()]
+
+	df_2 = pd.melt(df, id_vars = ['Name','User_note'], value_vars = ['Actor_1','Actor_2','Actor_3','Actor_4']).rename({"value":"Actor"},axis=1)
+	actor_options = [{"label":i,"value":i} for i in df_2['Actor'].unique()]
+
+	return director_options, actor_options
+
+
+@app.callback(
+	Output('poster_row','children'),
+	Input('intermediate-value','children'),
+	Input('type_checklist','value'),
+	Input('director_dropdown','value'),
+	Input('actor_dropdown','value'))
+def update_images(df_json,type_list,director_list,actor_list):
+	df = pd.read_json(df_json)
+	df = df[df['Type'].isin(type_list)]
+	if len(director_list) != 0 :
+		df = df[df['Director'].isin(director_list)]
+
+	if len(actor_list) !=0 :
+		actor_cols = [i for i in df.columns if 'Actor' in i]
+		df = df[(df[actor_cols].isin(actor_list)).any(axis="columns")]
+
+	urls = df.sort_values(['User_note','IMDB_note'],ascending=False)["Poster_url"].values
+	to_return = [html.Img(id="img_".format(i),src=urls[i],style=img_style) for i in range(len(urls))]
+	return to_return
---
+++ index.py
@@ -0,0 +1,49 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import data_scrapping
+
+
+# Connect to app.py file 
+from app import app
+from app import server
+
+# Connect to the app pages
+from apps import statistics, explorer
+
+app.layout = html.Div([
+	# Hidden div inside the app that stores the intermediate value
+	html.Div(id='intermediate-value', style={'display': 'none'}),
+	# Input for user id
+	dcc.Input(id = 'imdb_id', value="ur67399547" , type='text', style={"width":"20%"}),
+	html.Div([
+			dcc.Link('Statistics',href='/apps/statistics'),
+			dcc.Link('Movies explorer', href='/apps/explorer'),
+		], className="row"),
+	dcc.Location(id='url', refresh = False, pathname=''),
+	html.Div(id='page_content', children=[])
+])
+
+
+@app.callback(
+	Output('intermediate-value','children'),
+	Input('imdb_id','value'))
+def create_df(imdb_id):
+	df = data_scrapping.build_df(imdb_id.strip())
+	return df.to_json()
+
+
+@app.callback(
+	Output('page_content','children'),
+	Input('url','pathname'))
+def display_page(pathname):
+	if pathname == '/apps/statistics':
+		return statistics.layout
+	if pathname == '/apps/explorer':
+		return explorer.layout
+	else :
+		return "Please choose a Link"		
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
